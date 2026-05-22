3f0ea59e4385324e8970684defde86abfca9ee23
octavia0710 <tiurlasmariao11@gmail.com>
2026-05-22 14:22:07 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,7 @@ import streamlit as st
 
 st.title("🎈KICAU MANIA APP")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).")
 import streamlit as st
 
 color = st.color_picker("Pick A Color", "#00f900")
