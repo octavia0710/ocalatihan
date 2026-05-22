e08f9210a01ba49735be2e1240ef7deec7cadd59
octavia0710 <tiurlasmariao11@gmail.com>
2026-05-22 14:18:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ st.title("🎈KICAU MANIA APP")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+[runner]
+magicEnabled = false
